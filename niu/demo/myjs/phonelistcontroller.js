3b6f4bf35036df3ce907b6a6e81bc347ede863de
yao <303630573@qq.com>
2015-12-01 22:11:11 +0800

    ok

--- niu/demo/myjs/phonelistcontroller.js
+++ niu/demo/myjs/phonelistcontroller.js
@@ -5,7 +5,16 @@
 (function (angular) {
     "use strict";
 
-    angular.module('myApp').controller("phoneListCtrl",["$scope",'$q',function($scope,$q){
-        console.log("!!!");
+    angular.module('myApp').controller("phoneListCtrl",["$scope",'$q','$http',function($scope,$q,$http){
+       $http({
+           method:"GET",
+           url:"myjs/phones.json",
+
+       }).success(function(data){
+           $scope.phones = data.data;
+           console.log("数据接收成功!");
+       }).error(function(data){
+           console.log("服务器没给数据");
+       })
     }])
 })(angular)
